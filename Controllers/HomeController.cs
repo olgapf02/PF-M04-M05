c431f2e88231cc3d6e8736bf3946d7ff953b77ac
olgapf02 <olgapf02@gmail.com>
2023-05-29 10:23:47 +0200

    ya ta

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PF.Models;
+using Newtonsoft.Json;
+
+namespace PF.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index(){
+        const string url ="https://random.dog/woof.json";
+        var client = new HttpClient();
+        var response = client.GetAsync(url).Result;
+        var content = response.Content.ReadAsStringAsync().Result;
+        Perros imagen_de_perros = JsonConvert.DeserializeObject<Perros>(content);
+        return View( imagen_de_perros);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/Perros.cs
@@ -0,0 +1,7 @@
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Perros
+    {
+        public int fileSizeBytes { get; set; }
+        public string url { get; set; }
+    }
